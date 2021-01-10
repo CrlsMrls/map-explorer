d9645cfabdf21fddef854837f38b4857fd70b905
Carlos Morales <CrlsMrls@outlook.com>
2021-01-10 23:13:43 +0100

    Rubik like map - bad GPU performance

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
-console.log('hi there');
-
 const map = document.querySelector('#map');
 
-for (let i = 0; i < 2000; i++) {
+for (let i = 0; i < 100; i++) {
   const block = document.createElement('div');
   block.classList.add('block');
+  block.innerHTML = `
+    <div class="block__front"></div>
+    <div class="block__left"></div>
+    <div class="block__back"></div>
+    <div class="block__right"></div>
+    <div class="block__top"></div>
+
+  `;
   block.addEventListener('mouseover', () => {
-    block.classList.add('clicked');
+    block.classList.add('mouseover');
   });
   block.addEventListener('mouseleave', () => {
-    block.classList.remove('clicked');
+    block.classList.remove('mouseover');
   });
-  block.addEventListener('drag', () => {
-    block.classList.add('drag');
-    console.log('dragging');
+  block.addEventListener('click', () => {
+    block.classList.add('clicked');
   });
+
   map.appendChild(block);
 }
+
+let actualRotation = 0;
+
+document.querySelector('#rotate').addEventListener('click', () => {
+  const map = document.querySelector('#map');
+  map.classList.remove(`rotation-${actualRotation}`);
+  actualRotation = (actualRotation + 1) % 4;
+  map.classList.add(`rotation-${actualRotation}`);
+});
