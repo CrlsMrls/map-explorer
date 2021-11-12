4ed33c2c4b16d5ed4a74abb7de397825354f4c1f
Carlos Morales <CrlsMrls@outlook.com>
2021-11-12 23:23:23 +0100

    Add README.md and last changes

---
+++ README.md
@@ -0,0 +1,11 @@
+# Map explorer
+
+This repo is a very simple project that plays around with CSS transformations and animations.
+
+The main goal was to get some experience with [Parcel](https://parceljs.org/) bundler. Code is written in [TypeScript](https://www.typescriptlang.org/), CSS with [SASS](https://sass-lang.com/) and bundled all together with [Parcel](https://parceljs.org/). Configuration is really minimal, just a TS configuration file 👏👏👏👏.
+
+The page creates a 2500 `div` elements in a CSS Grid. It then transforms a few CSS properties with mouse click events on those elements. Zooming in/out (mouse wheel, buttons or ⬇️/⬆️ keyboard arrows) and rotating (button or ⬅️/➡️ keyboard arrows) are possible. Browser does not perform well with it.
+
+<img src="src/imgs/screenshot.png">
+
+On the positive side, my two nephews love playing with it.
--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,115 @@
 import '@material/mwc-icon-button';
 
 const map = document.querySelector('#map');
+const blocksPerSide = 50;
 
-for (let i = 0; i < 2500; i++) {
-  const block = document.createElement('div');
-  block.classList.add('block');
+let z = 0;
+let scale = 0.5;
 
-  block.addEventListener('mouseover', () => {
-    block.classList.add('mouseover');
-  });
-  block.addEventListener('mouseleave', () => {
-    block.classList.remove('mouseover');
-  });
+init();
 
-  const applyClicked = (event: PointerEvent) => {
-    if (event.pressure > 0) block.classList.add('clicked');
-  };
-  block.addEventListener('pointerdown', applyClicked);
-  block.addEventListener('pointerenter', applyClicked);
-
-  map.appendChild(block);
+function init() {
+  initBlocks();
+  initZoom();
+  initButtons();
+  initKeyboardEvents();
+  transformMap(z, scale);
 }
 
-let z = 45;
-let scale = 1;
+function initZoom() {
+  const handleZoomEvent = (event) => {
+    event.preventDefault();
+    transformMap(0, event.deltaY * -0.01);
+  };
 
-function transform(zInc: number, scaleInc: number): void {
+  map.addEventListener('wheel', handleZoomEvent);
+}
+
+function initBlocks() {
+  const totalBlocks = blocksPerSide * blocksPerSide;
+
+  for (let i = 0; i < totalBlocks; i++) {
+    const block = document.createElement('div');
+    block.classList.add('block');
+
+    block.addEventListener('mouseover', () => {
+      block.classList.add('mouseover');
+    });
+    block.addEventListener('mouseleave', () => {
+      block.classList.remove('mouseover');
+    });
+
+    const applyClicked = (event: PointerEvent) => {
+      if (event.pressure > 0) block.classList.add('clicked');
+    };
+    block.addEventListener('pointerdown', applyClicked);
+    block.addEventListener('pointerenter', applyClicked);
+
+    map.appendChild(block);
+  }
+}
+
+function transformMap(zInc: number, scaleInc: number): void {
   z += zInc;
-  if (scale + scaleInc > 0) {
+  const newScale = scale + scaleInc;
+  if (newScale > 0.5 && newScale < 4) {
     scale += scaleInc;
   }
-  const map = document.querySelector('#map');
-  map.setAttribute(
-    'style',
-    `transform: rotateX(55deg) rotateZ(${z}deg) scale(${scale})`
-  );
+
+  const columns = `grid-template-columns: repeat(${blocksPerSide}, min(1.5vh, 1vw));`;
+  const rotation = `transform: rotateX(55deg) rotateZ(${z}deg) scale(${scale});`;
+
+  map.setAttribute('style', columns + rotation);
 }
 
-document
-  .querySelector('#rotate_right')
-  .addEventListener('click', () => transform(45, 0));
+function initKeyboardEvents() {
+  const handleKeyboardEvent = (event) => {
+    if (event.defaultPrevented) {
+      return; // Do nothing if the event was already processed
+    }
 
-document
-  .querySelector('#rotate_left')
-  .addEventListener('click', () => transform(-45, 0));
+    switch (event.key) {
+      case 'Down': // IE/Edge specific value
+      case 'ArrowDown':
+        transformMap(0, -0.5);
+        break;
+      case 'Up': // IE/Edge specific value
+      case 'ArrowUp':
+        transformMap(0, 0.5);
+        break;
+      case 'Left': // IE/Edge specific value
+      case 'ArrowLeft':
+        transformMap(-45, 0);
+        break;
+      case 'Right': // IE/Edge specific value
+      case 'ArrowRight':
+        transformMap(45, 0);
+        break;
+      default:
+        return;
+    }
 
-document
-  .querySelector('#zoom_in')
-  .addEventListener('click', () => transform(0, 0.5));
+    // Cancel the default action to avoid it being handled twice
+    event.preventDefault();
+  };
 
-document
-  .querySelector('#zoom_out')
-  .addEventListener('click', () => transform(0, -0.5));
+  document.addEventListener('keydown', handleKeyboardEvent, true);
+}
+
+function initButtons() {
+  document
+    .querySelector('#rotate_right')
+    .addEventListener('click', () => transformMap(45, 0));
+
+  document
+    .querySelector('#rotate_left')
+    .addEventListener('click', () => transformMap(-45, 0));
+
+  document
+    .querySelector('#zoom_in')
+    .addEventListener('click', () => transformMap(0, 0.5));
+
+  document
+    .querySelector('#zoom_out')
+    .addEventListener('click', () => transformMap(0, -0.5));
+}
